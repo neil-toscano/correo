cfe3208d7e7f99b41e0db132017a366095789c62
neil <jacoborosseau@gmail.com>
2023-03-29 08:31:07 -0500

    inicio proyecto

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+const port=process.env.PORT;
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'neil.toscano.f@uni.pe',
+    pass: '20190318A.'
+  }
+});
+let mailOptions = {
+    from: 'neil.toscano.f@uni.pe',
+    to: 'jacoborosseau@gmail.com',
+    subject: 'Robotica',
+    text: 'Usando una api'
+  };
+  
+app.get('/enviarcorreo', function (req, res) {
+  res.send('Hello World');
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Correo electrónico enviado: ' + info.response);
+    }
+  });
+})
+
+app.listen(port);
+
+  
